ab8db7a0c5147f6b6a6b2fd46b6daeafcea2517d
Andre Esperança <andreluiz@lealapps.com>
2023-09-01 00:17:24 -0300

    feat: init create bottom sheet for create transaction

--- feature_manager/src/main/java/com/andreesperanca/feature_manager/screens/finance/BottomSheetCreateFinance.kt
+++ feature_manager/src/main/java/com/andreesperanca/feature_manager/screens/finance/BottomSheetCreateFinance.kt
@@ -1,7 +1,9 @@
 package com.andreesperanca.feature_manager.screens.finance
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -9,9 +11,20 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,30 +32,100 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.andreesperanca.ui_components.components.TitleMedium
 import com.andreesperanca.ui_components.theme.CamisaNoveTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomSheetCreateFinance(modifier: Modifier = Modifier)
 {
     Column(
         modifier = modifier
             .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    )
+        horizontalAlignment = Alignment.CenterHorizontally)
     {
-        var value by remember { mutableStateOf("") }
-        var valuePassword by remember { mutableStateOf("") }
+
+        val focusManager = LocalFocusManager.current
+        var showDatePickerDialog by remember {
+            mutableStateOf(false)
+        }
+        val datePickerState = rememberDatePickerState()
+        var selectedDate by remember {
+            mutableStateOf("")
+        }
+
+        var titleValue by remember { mutableStateOf("") }
+        var descriptionValue by remember { mutableStateOf("") }
+        var transitionValue by remember { mutableStateOf("") }
+
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    "Criar transação",
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            },
+            navigationIcon = {
+                IconButton(onClick = {
+                    showDatePickerDialog = !showDatePickerDialog
+                }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Localized description"
+                    )
+                }
+            }
+        )
+
+        if (showDatePickerDialog) {
+            DatePickerDialog(
+                onDismissRequest = { showDatePickerDialog = false },
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            datePickerState
+                                .selectedDateMillis?.let { millis ->
+                                    selectedDate = millis.toBrazilianDateFormat()
+                                }
+                            showDatePickerDialog = false
+                        }) {
+                        Text(text = "Escolher data")
+                    }
+                }) {
+                DatePicker(state = datePickerState)
+            }
+        }
+        OutlinedTextField(
+            value = selectedDate,
+            onValueChange = { },
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            label = {
+                Text("Date")
+            },
+            readOnly = true
+        )
+
 
         OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            value = value,
+            value = titleValue,
             onValueChange = {
-                value = it
+                titleValue = it
             },
             label = { Text(text = "Title") },
             placeholder = { Text(text = "Enter with title") },
@@ -52,9 +135,9 @@ fun BottomSheetCreateFinance(modifier: Modifier = Modifier)
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            value = value,
+            value = descriptionValue,
             onValueChange = {
-                value = it
+                descriptionValue = it
             },
             label = { Text(text = "Subtitle") },
             placeholder = { Text(text = "Enter with subtitle") },
@@ -64,15 +147,18 @@ fun BottomSheetCreateFinance(modifier: Modifier = Modifier)
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
-            value = value,
+            value = transitionValue,
             onValueChange = {
-                value = it
+                transitionValue = it
             },
             label = { Text(text = "Value") },
             placeholder = { Text(text = "Enter with value") },
         )
 
 
+        Button(onClick = {}) {
+            TitleMedium(text = "Olá Mundo")
+        }
     }
 }
 
@@ -82,4 +168,16 @@ fun BottomSheetCreateFinancePreview() {
     CamisaNoveTheme() {
         BottomSheetCreateFinance()
     }
+}
+
+fun Long.toBrazilianDateFormat(
+    pattern: String = "dd/MM/yyyy"
+): String {
+    val date = Date(this)
+    val formatter = SimpleDateFormat(
+        pattern, Locale("pt-br")
+    ).apply {
+        timeZone = TimeZone.getTimeZone("GMT")
+    }
+    return formatter.format(date)
 }
